55ebeba858be1bf82584e3401e08389af8841068
Kobus Van der Walt <kobusvdwalt9@gmail.com>
2019-11-09 20:58:40 +0200

    d

---
+++ Networks/Data/dataGenerator.py
@@ -0,0 +1,99 @@
+from __future__ import print_function
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np 
+import os
+import glob
+import skimage.io as io
+import skimage.transform as trans
+import threading
+from keras.utils import Sequence
+import math
+import random
+import cv2
+import albumentations
+
+class SnakeDataGenerator(Sequence):
+    def __init__(self, batch_size, source = 'train'):
+        self.batch_size = batch_size
+        self.source = source
+        self.path = 'E:/ML Dataset/Snake/train/'
+        self.sizeX = 256
+        self.sizeY = 256
+
+        classListFile = open("../Data/dataset/classList.txt", "r")
+        classList = classListFile.readlines()
+        classListFile.close()
+
+        self.classCount = len(classList)
+        self.classList = []
+        self.total = 0
+
+        self.composeAugmentation()
+
+        for i in range(0, len(classList)):
+            className = classList[i].split('|')[0]
+            classImagePathsFile = open("../Data/dataset/" + className + '_' + source + 'List.txt')
+            lines = classImagePathsFile.readlines()
+            classImagePathsFile.close()
+
+            self.classList.append(lines)
+            self.total += len(lines)            
+
+    def composeAugmentation(self):
+        self.augment = albumentations.Compose(
+            [
+                albumentations.HorizontalFlip()
+                #albumentations.Rotate(10, always_apply=True),
+                #albumentations.RandomSizedCrop((128, 640), self.sizeY, self.sizeX, 1, always_apply=True),
+                #albumentations.GridDistortion(always_apply=False),
+                #albumentations.IAAAffine(rotate=2, shear=5, always_apply=False),                
+                #albumentations.OpticalDistortion(),
+                #albumentations.ElasticTransform(alpha=64, sigma=32, always_apply=True, alpha_affine=0),
+                #albumentations.RandomBrightnessContrast(0.2, 0.2, always_apply=True),
+                #albumentations.Blur(always_apply=False)
+            ]
+        )
+
+    def __len__(self):
+        return math.ceil(self.total / self.batch_size)
+
+    def __getitem__(self, idx):
+        imageResult = np.zeros((self.batch_size, self.sizeY, self.sizeX, 3))
+        labelResult = np.zeros((self.batch_size, self.classCount))
+
+        for b in range(0, self.batch_size):            
+            randomClass = random.randint(0, self.classCount-1)
+
+            imageResult[b] = self.getImage(randomClass)
+            labelResult[b] = self.getLabel(randomClass)
+
+        return (imageResult, labelResult)
+
+    
+    def getImage(self, classNumber):
+        # Some of the images are corrupted on my disk
+        imageList = self.classList[classNumber]
+
+        image = None
+
+        while (image is None):
+            imagePath = imageList[random.randint(0, len(imageList)-1)]
+            image = cv2.imread(self.path + imagePath.rstrip("\n\r"))
+
+        image = cv2.resize(image, (self.sizeY, self.sizeX))
+        res = self.augment(image=image)
+        image = res['image']
+
+        seed = random.randint(0, 1000000)
+
+        cv2.imwrite(str(seed) + '.jpg', image)
+
+        image = (image - image.mean()) / (image.std() + 1e-8)
+
+        return image
+
+    def getLabel(self, classNumber):
+        label = np.zeros(self.classCount)
+        label[classNumber] = 1
+
+        return label
---
+++ Networks/Data/dataPreprocess.py
@@ -0,0 +1,73 @@
+import os
+import math
+
+# Poin this to the folder containing all the classes of snakes
+path = 'E:/ML Dataset/Snake/train/'
+
+class DataPreprocessing:
+    def __init__(self):
+        print('init_temp')
+        if (os.path.isdir('../Data/dataset/') == False):
+            os.mkdir('../Data/dataset/')
+
+    # This method generates a list of the classes of snake we have avaliable and how many images we have
+    def ClassList(self, classCount):
+        classListFile = open("../Data/dataset/classList.txt", "w")
+        imageListFile = open("../Data/dataset/imageList.txt", "w")
+
+        confirmedCount = 0
+        for i in range(1626):
+            if confirmedCount >= classCount:
+                break
+            if os.path.isdir(path + 'class-' + str(i)):
+                confirmedCount += 1
+                imageList = os.listdir(path + 'class-' + str(i))
+                classListFile.write('class-' + str(i) +'|'+ str(len(imageList)) + '\n')
+                for imageIterator in range(len(imageList)):
+                    imageListFile.write('class-' + str(i) + '/' + imageList[imageIterator] + '\n')
+
+                print('Current class : ' + str(i))
+
+        classListFile.close()
+        imageListFile.close()
+
+    # This method generates over/undersamples from each to create a proportional train/val/test set
+    def DataSplit(self, trainSplit=0.7, validateSplit=0.2, testSplit=0.1):
+        
+
+        classListFile = open("../Data/dataset/classList.txt", "r")
+        imageListFile = open("../Data/dataset/imageList.txt", "r")
+        classListLines = classListFile.readlines()
+        imageListLines = imageListFile.readlines()
+
+        total = 0
+        for i in range(len(classListLines)):        
+            className = classListLines[i].split('|')[0]
+
+            trainListFile = open("../Data/dataset/"+ className + "_trainList.txt", "w")
+            valListFile = open("../Data/dataset/"+ className + "_valList.txt", "w")
+            testListFile = open("../Data/dataset/"+ className + "_testList.txt", "w")
+            
+            classCount = int(classListLines[i].split('|')[1])
+            #classCount = min(classCountOriginal, 1000) # This is shit. We need a better way to sample equally from all classes
+            trainCount = math.floor( classCount * trainSplit)        
+            valCount = math.floor( classCount * validateSplit)
+            testCount = math.floor( classCount * testSplit)
+
+            for imageIterator in range(total, total+trainCount, 1):
+                trainListFile.write(imageListLines[imageIterator])
+            
+            for imageIterator in range(total+trainCount, total+trainCount+valCount, 1):
+                valListFile.write(imageListLines[imageIterator])
+
+            for imageIterator in range(total+trainCount+valCount, total+classCount, 1):
+                testListFile.write(imageListLines[imageIterator])
+
+            total += classCount
+
+            trainListFile.close()
+            valListFile.close()
+            testListFile.close()
+
+        classListFile.close()
+        imageListFile.close()
